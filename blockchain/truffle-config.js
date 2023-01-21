c9ea56dc407831b12d94dec4c2f8ece520d1620c
A S Kareshmmaa <kareshmmaa2001@gmail.com>
2023-01-22 02:08:49 +0530

    sample

---
+++ blockchain/truffle-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+      gas: 6000000,
+      from: '0x945587b7B22D4Ef4D52E92e2b010dF135b23cFa0',
+    },
+  },
+  contracts_directory: './testing/',
+  contracts_build_directory: './src/build/',
+  compilers: {
+    solc: {
+      version: "0.6.6",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 100
+        }
+      }
+    }
+  }
+}
